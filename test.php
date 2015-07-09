34e16e0a5430f1ad272fa88fcf97c8655119e767
leexikang <leexikang@gmail.com>
2015-07-09 13:45:09 +0630

    change to oop design

--- test.php
+++ test.php
@@ -8,29 +8,75 @@
 //imagecopyresized($nm, $image, 0,0,0,0,100,75,$ox,$oy);
 //imagejpeg($nm, "images/1-thumb.jpg");
 
-Class Thumbnails{
+class ImageManipulation{
 
-  public $dir;
+  protected $thumbnails;
+  protected $downgrade;
+  protected $dir;
 
-  public function __construct($dir){
-    $this->dir = $dir;
+   public function __construct(CreateThumbnails $thumbnails,  DowngradImage $downgrade,  $dir){
+
+      $this->thumbnails = $thumbnails;
+      $this->downgrade = $downgrade;
+      $this->dir = $dir;
+
+   }
+
+  public function make($xlength, $quality){
+
+    $this->createPath();
+
+    foreach(scandir($this->dir) as $file){
+
+      if( $file != "." && $file != ".." && preg_match('/[.](jpg)|(gif)|(png)$/', $file)){
+
+        $this->thumbnails->make($file, $xlength, $this->dir, $this->getThumnailsPath());
+        $this->downgrade->make($file, $quality, $this->dir, $this->getDowngradePath());
+        print($file . " operation completed \n");
+      }
+
+    }
   }
-  public function createThumbnail($file, $nx){
 
-    $image = imagecreatefromjpeg($this->dir . "/" . $file);
-    list($mx, $my) = $this->getXY($image);
-    $ny = floor( $my * ( $nx / $mx));
-    $nm = imagecreatetruecolor($nx, $ny);
-    imagecopyresized($nm, $image, 0, 0, 0, 0, $nx, $ny, $mx, $my);
-    imagejpeg($nm, $this->getPath() . "/" . $file);
+  protected function createPath(){
+
+    foreach($this->getPathByArray() as $path){
+      if(!file_exists($path)){
+        if(!mkdir($path)){
+          die("can't create " . $path);
+        }
+      }
+    }
+  }
+
+  protected function createThumbnailsPath(){
+
+    $this->thumbnails->returnDestination();
 
   }
 
-  public function getPath(){
+  protected function getThumnailsPath(){
 
-  return $this->dir . "/thumbnails";
+    return $this->dir . "/" . $this->thumbnails->returnDestination();
 
   }
+
+  protected function getDowngradePath(){
+
+    return $this->dir . "/" . $this->downgrade->returnDestination();
+
+  }
+
+  protected function getPathByArray(){
+
+    return [ $this->getThumnailsPath(),
+      $this->getDowngradePath()
+      ];
+  }
+
+};
+
+Class DowngradImage{
 
   protected function createPath(){
 
@@ -40,8 +86,41 @@ Class Thumbnails{
       }
     }
   }
+  public function returnDestination(){
+
+    return "downgrad";
+
+  }
+
+  public function make($file, $quality, $source, $destination){
+
+    $image = imagecreatefromjpeg($source . "/" . $file);
+    $dimage = imagejpeg($image, $destination . "/" . $file , $quality);
+
+  }
 
 
+
+}
+
+Class CreateThumbnails{
+
+  public function make($file, $nx, $source, $destination){
+
+    $image = imagecreatefromjpeg($source . "/" . $file);
+    list($mx, $my) = $this->getXY($image);
+    $ny = floor( $my * ( $nx / $mx));
+    $nm = imagecreatetruecolor($nx, $ny);
+    imagecopyresized($nm, $image, 0, 0, 0, 0, $nx, $ny, $mx, $my);
+    imagejpeg($nm, $destination  . "/" . $file, 30);
+
+  }
+
+  public function returnDestination(){
+
+    return "thumbnails";
+
+  }
 
   public function getXY($image){
 
@@ -51,22 +130,14 @@ Class Thumbnails{
 
   }
 
-  public function make($xlength){
-
-    $files = scandir($this->dir);
-    $this->createPath();
-  foreach($files as $file){
-    if( $file != "." && $file != ".." && preg_match('/[.](jpg)|(gif)|(png)$/', $file)){
-      $this->createThumbnail($file, $xlength);
-      print($file . "has been created \n");
-    }
-
-  }
-  }
 
 
 }
 
-$thumnail = new Thumbnails("images");
+/* echo "Please give the path your want to create thumbnails ? \n";
+$dir = readline();
+$thumnail = new Thumbnails($dir);
 $thumnail->make(200);
-
+ */
+$image = new ImageManipulation(new CreateThumbnails, new DowngradImage, "images");
+$image->make(100, 30);
